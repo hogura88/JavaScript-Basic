c076cf90e7f1b492fb8dfb6986a489898a39b9ab
hogura88 <hogura88@gmail.com>
2024-07-05 15:45:37 +0900

    review

--- js2/dom.js
+++ js2/dom.js
@@ -3,3 +3,6 @@ console.log(document.body);
 
 console.log(document.getElementById('first-list'));
 console.log(document.getElementsByClassName('heading'));
+
+const headings = document.getElementsByClassName('heading');
+
