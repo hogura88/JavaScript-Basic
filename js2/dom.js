034308b307cd6d18e97b99cc32d712d479c8a5ce
hogura88 <hogura88@gmail.com>
2024-07-07 15:15:14 +0900

    review

--- js2/dom.js
+++ js2/dom.js
@@ -9,3 +9,7 @@ const headings = document.getElementsByClassName('heading');
 for (let i = 0; i < headings.length; i++) {
     console.log(headings[i]);
 }
+
+console.log(document.querySelector('h1'));
+console.log(document.querySelector('#seconde-heading'));
+console.log(document.querySelector('.list'));
