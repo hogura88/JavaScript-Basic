cf46d22b72e5b700439f4f5b6f9076e5196e11cb
hogura88 <hogura88@gmail.com>
2024-07-12 15:46:37 +0900

    review

--- js2/event.js
+++ js2/event.js
@@ -3,3 +3,6 @@ const btn = document.getElementById('output-btn');
 btn.addEventListener('click', () => {
     console.log('クリックされました！');
 });
+
+const addBtn = document.getElementById('add-btn');
+const parentList = document.getElementById('parent-list')
