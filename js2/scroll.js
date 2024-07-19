dff1cb306abf6011b501f2d3028036a3ac9b3f46
hogura88 <hogura88@gmail.com>
2024-07-19 23:10:10 +0900

    re

--- js2/scroll.js
+++ js2/scroll.js
@@ -2,5 +2,10 @@ const backBtn = document.getElementById('back-btn');
 
 window.addEventListener('scroll', () => {
     const scrollVallue = document.scrollingElement.scrollTop;
-    
-})
+    if (scrollVallue >= 300) {
+        backBtn.style.display = 'inline';
+    }
+    else {
+        backBtn.style.display = 'none';
+    }
+});
